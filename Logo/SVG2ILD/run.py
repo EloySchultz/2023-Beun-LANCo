bef584b8a02b990e201b63f1c85c9716fb984c47
Schultz <e.w.r.schultz@student.tue.nl>
2023-05-07 11:52:47 +0200

    Add and move laser content

---
+++ Logo/SVG2ILD/run.py
@@ -0,0 +1,17 @@
+import video_converter
+import image_converter
+import os
+
+
+
+
+#Convert your videos here!   ["", Path to SVG sequence, Path to output file]
+# name="Individual_scale"
+# name="Writing_effect"
+# video_converter.main(["",os.path.join(os.getcwd(),"content","SVG_sequences",name),os.path.join(os.getcwd(),"ILDA",name+".ild")])
+
+
+##Convert your static images here
+name = "Logo_Teslan_Outline_Borders_Rainbow"
+image_converter.main(["", os.path.join(os.getcwd(),"content","SVG_static",name+".svg"),os.path.join(os.getcwd(),"ILDA",name+".ild")])
+
