cdcf907a647e3d2117e310cf2a1534e33363f731
Schultz <e.w.r.schultz@student.tue.nl>
2023-05-20 17:09:29 +0200

    Add LANCo animation

--- Logo/SVG2ILD/run.py
+++ Logo/SVG2ILD/run.py
@@ -6,12 +6,17 @@ import os
 
 
 #Convert your videos here!   ["", Path to SVG sequence, Path to output file]
-# name="Individual_scale"
-# name="Writing_effect"
-# video_converter.main(["",os.path.join(os.getcwd(),"content","SVG_sequences",name),os.path.join(os.getcwd(),"ILDA",name+".ild")])
+name="LANCo"
+#name="Writing_effect"
+video_converter.main(["",os.path.join(os.getcwd(),"content","SVG_sequences",name),os.path.join(os.getcwd(),"ILDA",name+".ild")])
 
 
 ##Convert your static images here
-name = "Logo_Teslan_Outline_Borders_Rainbow"
-image_converter.main(["", os.path.join(os.getcwd(),"content","SVG_static",name+".svg"),os.path.join(os.getcwd(),"ILDA",name+".ild")])
+#name = "Logo_Teslan_Outline_Borders_Rainbow"
+#image_converter.main(["", os.path.join(os.getcwd(),"content","SVG_static",name+".svg"),os.path.join(os.getcwd(),"ILDA",name+".ild")])
 
+
+#
+# name = "CY-Logo-White-Transparent_test"
+# image_converter.main(["", os.path.join(os.getcwd(),"content","SVG_static",name+".svg"),os.path.join(os.getcwd(),"ILDA",name+".ild")])
+#
