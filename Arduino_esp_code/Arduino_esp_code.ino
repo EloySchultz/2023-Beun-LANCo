3f81ea8f873d792b32a04038b23c0df219fd3d53
Schultz <e.w.r.schultz@student.tue.nl>
2023-05-28 14:01:03 +0200

    Useless commit

--- Arduino_esp_code/Arduino_esp_code.ino
+++ Arduino_esp_code/Arduino_esp_code.ino
@@ -3,7 +3,7 @@
 #include <FastLED.h>
 
 // CONFIG //
-#define SEINPAAL_4 1;  //Put here which seinpaal you are uploading to.
+#define LEDBEAM_4 1;  //Put here which seinpaal you are uploading to.
 #include "devices.h"
 
 //END OF CONFIG//
